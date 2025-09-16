69bb8b54d8f23dfe6295b551d5d2120b69337a8d
nqhuy0613 <qhuy061305@gmail.com>
2025-09-16 15:10:59 +0700

    70

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,7 +1,19 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String email, password, fullName, address, phone;
 
     public long getId() {
